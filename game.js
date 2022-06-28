7f1625d927ef823beffeae1e176c02546e22223c
wozller <brian.sanchez.wozller@gmail.com>
2022-06-28 15:50:40 -0400

    Create the playRound function
    Create a function that plays a single round of Rock Paper Scissors. The
    function takes two parameters - the playerSelection and
    computerSelection - and then returns a string that declarees the winner
    of the round like so: "You Lose! Paper beats Rock".

--- game.js
+++ game.js
@@ -11,3 +11,53 @@ function computerPlay() {
         return "Scissors";
 
 } // End of computerPlay function.
+
+function playRound(playerSelection, computerSelection) {
+
+    let outcome = "";
+
+    outcome += "Player played " + playerSelection + "\n";
+    outcome += "Computer played " + computerSelection + "\n";
+
+    switch (playerSelection) {
+
+        case "Rock":
+
+            if (computerSelection == "Rock")
+                outcome += "Tie! Rock ties with Rock";
+            else if (computerSelection == "Paper")
+                outcome += "You Lose! Paper beats Rock";
+            else if (computerSelection == "Scissors")
+                outcome += "You Win! Rock beats Scissors";
+
+            break;
+
+        case "Paper":
+
+            if (computerSelection == "Rock")
+                outcome += "You Win! Paper beats Rock";
+            else if (computerSelection == "Paper")
+                outcome += "Tie! Paper ties with Paper";
+            else if (computerSelection == "Scissors")
+                outcome += "You Lose! Scissors beats Paper";
+
+            break;
+
+        case "Scissors":
+
+            if (computerSelection == "Rock")
+                outcome += "You Lose! Rock beats Scissors";
+            else if (computerSelection == "Paper")
+                outcome += "You Win! Scissors beat Paper";
+            else if (computerSelection == "Scissors")
+                outcome += "Tie! Scissors ties with Scissors";
+
+            break;
+
+    } // End of switch statement.
+
+    outcome += "\n";
+
+    return outcome;
+
+} // End of playRound function.
